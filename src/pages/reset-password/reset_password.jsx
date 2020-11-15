b95fd8d6347d02757a2f5eeaddcedc0890204126
Imran01000 <imshaikh01000@gmail.com>
2020-11-15 21:48:39 +0530

    MODIFIED: Added validation to form

--- src/pages/reset-password/reset_password.jsx
+++ src/pages/reset-password/reset_password.jsx
@@ -6,36 +6,79 @@ import {
 import { Link } from 'react-router-dom'
 import './reset_password.scss'
 export class reset_password extends Component {
+    
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             pwd:"",
+             pwdError:"",
+             pwdErrorText:""
+        }
+    }
+    
+    
+    validateForPassword = e => {
+        const regexForPwd = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/g;
+        const inputData = e.target.value;
+        if(regexForPwd.test(inputData)){
+           this.setState({
+            pwdError: false, pwdErrorText: "" 
+           })
+           console.log(inputData);
+        }else{
+            this.setState({
+                [e.target.name]: "",
+                pwdError: true,
+                pwdErrorText: "Minimum 6 characters required"
+            },
+            ()=>{
+                console.log(this.state.pwdErrorText);
+            }
+            );
+        }
+        
+    }
     render() {
         return (
             <Container className="main-reset">
-            <Card className="form-reset">
-                <Card className="form-head-reset">
-                    <h3 className="fundoo-reset">Fundoo note</h3>
+            <form onSubmit="">
+                <Card className="form-reset">
+                    <Card className="form-head-reset">
+                        <h3 className="fundoo-reset">Fundoo note</h3>
+                    </Card>
+                    <div className="text-reset">
+                        <b>Set new password!!</b><br/> 
+                        <span>be happy</span>
+                    </div>
+                    <div className="text-field-reset">
+                        <div id="newpassword-field-reset">
+                            <TextField  
+                                label="New password"
+                                type="password"
+                                size="small"
+                                variant="outlined" 
+                                onBlur={this.validateForPassword}
+                                error={this.state.pwdError.length > 0}
+                                helperText={this.state.pwdErrorText}/>
+                        </div>
+                        <div id="confirm-field-reset">
+                            <TextField  
+                                label="Confirm password"
+                                type="password"
+                                size="small"
+                                variant="outlined"
+                                onBlur={this.validateForPassword}
+                                error={this.state.pwdError.length > 0}
+                                helperText={this.state.pwdErrorText} />
+                        </div>
+                    </div>
+                    <div className="buttons-reset">
+                        <Link to="/registration">Create account</Link>
+                        <Button variant="contained">Confirm</Button>
+                    </div>
                 </Card>
-                <div className="text-reset">
-                    <b>Set new password!!</b><br/> 
-                    <span>be happy</span>
-                </div>
-                <div className="text-field-reset">
-                    <div id="newpassword-field-reset">
-                        <TextField  
-                            label="New password"
-                            size="small"
-                            variant="outlined" /><br/>
-                    </div>
-                    <div id="confirm-field-reset">
-                        <TextField  
-                            label="Confirm password"
-                            size="small"
-                            variant="outlined" /><br/>
-                    </div>
-                </div>
-                <div className="buttons-reset">
-                    <Link to="/registration">Create account</Link>
-                    <Button variant="contained">Confirm</Button>
-                </div>
-            </Card>
+            </form>
         </Container>
         )
     }
