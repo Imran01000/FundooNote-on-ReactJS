d71b85d00f69b8a2c99029a9917e654f2268455d
Imran01000 <imshaikh01000@gmail.com>
2020-11-28 10:52:40 +0530

    MODIFIED: Added display notes functionality by applying map function

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Card, CardContent, Typography,
-    CardActions, Grid, IconButton, GridList
+    CardActions, Grid, IconButton, Menu,
+    MenuItem
 } from '@material-ui/core'
 import AddAlertOutlinedIcon from '@material-ui/icons/AddAlertOutlined';
 import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
@@ -10,15 +11,20 @@ import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined';
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
 import { Tooltip } from '@material-ui/core';
 import { withStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import pinBeforeClick from '../../images/pinBeforeClick.png'
 
 const styles ={
     root:{
-        width:"22%",
-        marginTop:"50px",
-        marginLeft:"20px",
-        height:"5%",
+        flexDirection:"row",
+        marginLeft:"280px"
+
     },
-    
+    card:{
+        width:"20%",
+        marginTop:"50px",
+        height:"5%",
+        marginLeft:"20px"
+    },    
 };
 
 export class Notes extends Component {
@@ -26,62 +32,105 @@ export class Notes extends Component {
         super(props)
     
         this.state = {
-             
+             Notes:props.allNotes,
+             anchor:null
         }
     }
-    
-    
+     handleClick = (event) => {
+        this.setState({
+            anchor:event.currentTarget
+        })
+      };
+      handleClose = () =>{
+        this.setState({
+            anchor:null
+        })
+      }
     render() {
         const {classes} = this.props;
-        return (
-                <Grid  container direction="row">             
-                <Card className={classes.root}>
-                    <CardContent>
-                    <h3>first Demo</h3>
-                        <Typography>
-                            good afternoon!
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Grid  container
-                                direction="row"
-                                justify="space-around"
-                                alignItems="flex-start">
-                            <Tooltip title="Remind me">
-                                <IconButton size="small">
-                                    <AddAlertOutlinedIcon fontSize="small"/>
-                                </IconButton>
-                            </Tooltip>
-                            <Tooltip title="Collaborator">
-                                <IconButton size="small">
-                                    <PersonAddOutlinedIcon fontSize="small"/>
-                                </IconButton>
-                            </Tooltip>
-                            <Tooltip title="Change color">
-                                <IconButton size="small">
-                                    <PaletteOutlinedIcon fontSize="small"/>
-                                </IconButton>
-                            </Tooltip>
-                            <Tooltip title="Add image">
-                                <IconButton size="small">
-                                    <ImageOutlinedIcon fontSize="small"/>
-                                </IconButton>
-                            </Tooltip>
-                            <Tooltip title="Archive">
-                                <IconButton size="small">
-                                    <ArchiveOutlinedIcon fontSize="small"/>
-                                </IconButton>
-                            </Tooltip>
-                            <Tooltip title="More">
-                                <IconButton size="small">
-                                    <MoreVertOutlinedIcon fontSize="small"/>
-                                </IconButton>
-                            </Tooltip>
-                        </Grid>
-                    </CardActions>
-                </Card>  
-                </Grid> 
+        console.log("All notes", this.props.allNotes)
+        console.log("from child", this.state.title) 
+
+        var menu = <div>
+                        <Menu
+                            id="menu"
+                            anchorEl={this.state.anchor}
+                            keepMounted
+                            open={Boolean(this.state.anchor)}
+                            onClose={this.handleClose}
+                            >
+                            <MenuItem onClick={this.handleClose} >Delete note</MenuItem>
+                            <MenuItem onClick={this.handleClose}>Add labels</MenuItem>
+                        </Menu>
+                    </div>
+            var displayNotes = this.props.allNotes.map((notes)=>{
             
+            return (            
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <Grid container
+                            direction="row"
+                            justify="space-between"
+                            alignItems="flex-start">
+                                <h3>{notes.title}</h3>
+                                <Tooltip title="Pin note">
+                                    <IconButton size="small">
+                                        <img src={pinBeforeClick}/>
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                            <Typography>
+                                {notes.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Grid  container
+                                    direction="row"
+                                    justify="space-around"
+                                    alignItems="flex-start">
+                                <Tooltip title="Remind me">
+                                    <IconButton size="small">
+                                        <AddAlertOutlinedIcon fontSize="small"/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Collaborator">
+                                    <IconButton size="small">
+                                        <PersonAddOutlinedIcon fontSize="small"/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Change color">
+                                    <IconButton size="small">
+                                        <PaletteOutlinedIcon fontSize="small"/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Add image">
+                                    <IconButton size="small">
+                                        <ImageOutlinedIcon fontSize="small"/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Archive">
+                                    <IconButton size="small">
+                                        <ArchiveOutlinedIcon fontSize="small"/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="More">
+                                    <IconButton size="small" aria-controls="menu" aria-haspopup="true"
+                                    onClick={this.handleClick}>
+                                        <MoreVertOutlinedIcon fontSize="small"/>
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                        </CardActions>
+                    </Card> 
+            )
+        })
+        
+        return(
+            <div className={classes.root}>
+                <Grid container direction="row">
+                    {displayNotes}
+                </Grid>
+            </div>
         )
     }
 }
