4fe836d7da68c60f467ef176df92a72941fef734
Imran01000 <imshaikh01000@gmail.com>
2020-12-06 20:25:29 +0530

    MODIFIED: Added API'S for pin, archive

--- src/services/note-service.js
+++ src/services/note-service.js
@@ -19,3 +19,23 @@ export const addColorNotes = (data, token) =>{
   const URL = `http://fundoonotes.incubation.bridgelabz.com/api/notes/changesColorNotes?access_token=${token}`;
   return axios.post(`${URL}`, data)
 };
+
+export const trashNotes = (data, token) =>{
+  const URL = `http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes?access_token=${token}`;
+  return axios.post(`${URL}`, data)
+};
+
+export const archiveNotes = (data, token)=>{
+  const URL = `http://fundoonotes.incubation.bridgelabz.com/api/notes/archiveNotes?access_token=${token} `
+  return axios.post(`${URL}`, data)
+};
+
+export const PinUnpinNotes = (data, token)=>{
+  const URL = `http://fundoonotes.incubation.bridgelabz.com/api/notes/pinUnpinNotes?access_token=${token}`;
+  return axios.post(`${URL}`, data)
+};
+
+export const deleteNoteForever = (data, token)=>{
+  const URL = `http://fundoonotes.incubation.bridgelabz.com/api/notes/deleteForeverNotes?access_token=${token}`;
+  return axios.post(`${URL}`, data);
+};
