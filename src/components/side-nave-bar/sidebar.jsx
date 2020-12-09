9307f62fc96e18ac1156e4a77915e3d6509f4448
Imran01000 <imshaikh01000@gmail.com>
2020-12-09 09:47:32 +0530

    MODIFIED: converted functional component to class component

--- src/components/side-nave-bar/sidebar.jsx
+++ src/components/side-nave-bar/sidebar.jsx
@@ -1,38 +1,45 @@
-import React, {useState,setState} from 'react'
-import { 
+import React, { Component } from 'react'
+import {
     AppBar, Toolbar, IconButton,
     Menu, MenuItem, Typography, InputBase,
     Drawer, List, ListItem, ListItemText, ListItemIcon,
-    Button
+    Button,
 } from '@material-ui/core'
 import EmojiObjectsOutlinedIcon from '@material-ui/icons/EmojiObjectsOutlined';
 import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import ArchiveOutlinedIcon from '@material-ui/icons/ArchiveOutlined';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 import './sidebar.scss'
-const UseStyles = makeStyles({
-    root:{
-        
+import LabelDailog from '../Label/LabelDailog'
+import LabelOutlinedIcon from '@material-ui/icons/LabelOutlined';
+import { getAllLabelList } from '../../services/label-service'
+import { withStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+
+const styles = {
+    root: {
+
     },
     drawer: {
-        width: "200px",
         flexShrink: 0,
         whiteSpace: "nowrap",
         display: "flex",
-      }
-
-})
+    },
+    list: {
+        width: "250px"
+    }
+}
 
 const theme = createMuiTheme({
     overrides: {
         MuiDrawer: {
             paper: {
-                top: '10%',
+                top: '9.4%',
+
             },
-            paperAnchorDockedRight:{
-              borderRight: 20
+            paperAnchorDockedRight: {
+                borderRight: 20,
             }
         },
         MuiPaper: {
@@ -40,80 +47,133 @@ const theme = createMuiTheme({
                 backgroundColor: 'transparent'
             }
         }
-
     }
 })
 
 
-function Sidebar(props) {
-    const classes = UseStyles();
-    const [open, setOpen] = useState(props.isOpen);    
-    
-    console.log("value getting here!",props.isOpen);
 
-    return (
-        <div>
-            <ThemeProvider  theme={theme}>
-            <div className={classes.root} >
+export class sidebar extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            allLabelNote: [],
+            isOpenDailog: false,
+            userId: ''
+        }
+    }
+    componentWillMount = () => {
+        let localStorageData = JSON.parse(localStorage.getItem('userdata'));
+        let userToken = localStorageData.userToken
+        getAllLabelList(userToken).then(
+            result => {
+                this.setState({
+                    allLabelNote: result.data.data.details
+                })
+            }
+        )
+    }
+    toOpenLabelDialog = () => {
+        this.setState({
+            isOpenDailog: true,
+            userId: this.props.userId
+        })
+        this.componentWillMount()
+    }
+
+    toCloseLabelDialog = () => {
+        this.setState({
+            isOpenDailog: false
+        })
+        this.componentWillMount()
+    }
+
+
+    render() {
+
+        const { classes } = this.props;
+        console.log('222', this.props.userId)
+
+        var displayLabel = this.state.allLabelNote.map((labelData) => {
+
+            return (
+                <ListItem button className="list-item"
+                >
+                    <ListItemIcon>
+                        <LabelOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                        {labelData.label}
+                    </ListItemText>
+                </ListItem>
+            )
+        })
+
+        return (
+            <div>
+                <ThemeProvider theme={theme}>
+                    <div className={classes.root} >
                         <Drawer variant="persistent" className={classes.drawer}
-                         open={props.isOpen} onClose={false}>
-                            <List className="list">
-                                <ListItem>
-                                    <Button className="buttons" style={{paddingRight:"30px"}}>
+                            open={this.props.isOpen} onClose={this.props.closeDrawer}>
+                            <List className={classes.list}>
+                                <ListItem button className="list-item"
+                                    onClick={() => window.location.href = "/dash-board"}>
                                     <ListItemIcon>
-                                        <EmojiObjectsOutlinedIcon/>
+                                        <EmojiObjectsOutlinedIcon />
                                     </ListItemIcon>
-                                     <ListItemText>
-                                         Notes
-                                     </ListItemText>
-                                     </Button>
+                                    <ListItemText>
+                                        Notes
+                                    </ListItemText>
                                 </ListItem>
-                                <ListItem>
-                                    <Button className="buttons">
+                                <ListItem button className="list-item"
+                                    onClick={() => window.location.href = "/reminder"}>
                                     <ListItemIcon>
-                                        <NotificationsNoneOutlinedIcon/>
+                                        <NotificationsNoneOutlinedIcon />
                                     </ListItemIcon>
-                                     <ListItemText>
-                                         Reminders
+                                    <ListItemText>
+                                        Reminders
                                      </ListItemText>
-                                     </Button>
                                 </ListItem>
-                                <ListItem>
-                                    <Button className="buttons">
+                                {displayLabel}
+                                <ListItem button className="list-item"
+                                    onClick={this.toOpenLabelDialog}>
                                     <ListItemIcon>
-                                        <EditOutlinedIcon/>
+                                        <EditOutlinedIcon />
                                     </ListItemIcon>
-                                     <ListItemText>
-                                         Edit labels
+                                    <ListItemText>
+                                        Edit labels
                                      </ListItemText>
-                                     </Button>
                                 </ListItem>
-                                <ListItem>
-                                    <Button className="buttons">
+                                <ListItem button className="list-item"
+                                    onClick={() => window.location.href = "/archive"}>
                                     <ListItemIcon>
-                                        <ArchiveOutlinedIcon/>
+                                        <ArchiveOutlinedIcon />
                                     </ListItemIcon>
-                                     <ListItemText>
-                                     Archive
+                                    <ListItemText>
+                                        Archive
                                      </ListItemText>
-                                     </Button>
                                 </ListItem>
-                                <ListItem>
-                                    <Button className="buttons">
+                                <ListItem button className="list-item"
+                                    onClick={() => window.location.href = "/trash"}>
                                     <ListItemIcon>
-                                        <DeleteOutlinedIcon/>
+                                        <DeleteOutlinedIcon />
                                     </ListItemIcon>
-                                     <ListItemText>
-                                     Trash
+                                    <ListItemText>
+                                        Trash
                                      </ListItemText>
-                                     </Button>
                                 </ListItem>
                             </List>
                         </Drawer>
-                </div>
-            </ThemeProvider>
-        </div>
-    )
+                    </div>
+                </ThemeProvider>
+                <LabelDailog isOpen={this.state.isOpenDailog} toClose={this.toCloseLabelDialog}
+                    id={this.state.userId} />
+            </div>
+        )
+    }
 }
 
-export default Sidebar
+export default withStyles(styles)(sidebar)
+
+
